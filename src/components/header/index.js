2b79d314cc29219ec2b2bd2356c62d50fae1d90b
Dam Van Huan <huan.dam@muaban.net>
2023-08-23 23:22:23 +0700

    Add header

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,5 +1,13 @@
-const Header = () => {
-  return <></>;
+import { HeaderWrapper } from "./style";
+import Logo from "logo.svg";
+
+const Header = ({ user }) => {
+  return (
+    <HeaderWrapper>
+      <div>Hi, {user.username || user.email}. Welcome to movie hub</div>
+      <img alt="logo" src={Logo} />
+    </HeaderWrapper>
+  );
 };
 
 export default Header;
